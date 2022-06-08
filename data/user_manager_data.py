c6859ff00490c8a2c8f0eb7e2355ffc0a18af8f0
hezhansheng <631045496@qq.com>
2022-06-08 15:47:18 +0800

    自动化测试框架

---
+++ data/user_manager_data.py
@@ -0,0 +1,10 @@
+
+
+class UserManagerData():
+
+    add_user_data = {
+        "errno":0,
+        "username":"tsetr25",
+        "password":"123456",
+        "new_username":"tsetr35"
+    }
---
+++ setting.py
@@ -0,0 +1,7 @@
+
+
+#1.配置公共的接口地址
+BASE_URL = "http://121.196.13.152:8080"
+
+#2.配置登录名和密码
+LOGIN_INFO = {"username":"admin123","password":"admin123"}
